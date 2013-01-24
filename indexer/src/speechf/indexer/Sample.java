a6eae3a1148877f3a1ca09d0e6bf9caf41a86c32
shiva2110 <shiva2110@gmail.com>
2013-01-24 09:11:43 -0700

    adding sample.java changes

--- indexer/src/speechf/indexer/Sample.java
+++ indexer/src/speechf/indexer/Sample.java
@@ -1,6 +1,10 @@
 package speechf.indexer;
 
+import java.io.File;
+
+import edu.cmu.sphinx.frontend.util.AudioFileDataSource;
 import edu.cmu.sphinx.recognizer.Recognizer;
+import edu.cmu.sphinx.result.Result;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
 
 public class Sample {
@@ -16,5 +20,15 @@ public class Sample {
 		
 		 Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
 	     recognizer.allocate();
+	     
+	     AudioFileDataSource audioFileSource = (AudioFileDataSource)cm.lookup("audioFileDataSource");
+	     File file = new File("/Users/shiva2110/Documents/voice.wav");
+	     audioFileSource.setAudioFile(file, "");
+	     
+	     
+	     Result result = recognizer.recognize();
+	     System.out.println(result.getBestFinalResultNoFiller());
+	     
+	     
 	}
 }
