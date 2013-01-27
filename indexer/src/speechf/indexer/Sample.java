6c0abffa4988c13532c34ffa0036c34a756afe05
shiva2110 <shiva2110@gmail.com>
2013-01-26 20:06:03 -0700

    exploring sphinx with some samples

--- indexer/src/speechf/indexer/Sample.java
+++ indexer/src/speechf/indexer/Sample.java
@@ -6,7 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 
-import edu.cmu.sphinx.frontend.util.AudioFileDataSource;
+import edu.cmu.sphinx.frontend.Data;
 import edu.cmu.sphinx.frontend.util.StreamDataSource;
 import edu.cmu.sphinx.recognizer.Recognizer;
 import edu.cmu.sphinx.result.Result;
@@ -24,15 +24,22 @@ public class Sample {
 		}
 		
 		 
-	     
+	    System.out.println("running it now ");
 		StreamDataSource audioFileSource = (StreamDataSource)cm.lookup("streamDataSource");
+		//InputStream inputStream = new FileInputStream(new File("/projects/workrepo/speech-F/sphinx4-1.0beta6-src/sphinx4-1.0beta6/src/test/edu/cmu/sphinx/result/test/green.wav"));
+		//InputStream inputStream = new FileInputStream(new File("/Users/shiva2110/Downloads/PDA/PDAm/16k/013/PDAm13_036_2.wav"));
 		InputStream inputStream = new FileInputStream(new File("/Users/shiva2110/Downloads/male.wav"));
-	     audioFileSource.setInputStream(inputStream, "");
-	     
+	    audioFileSource.setInputStream(inputStream, "");	     
 	     Recognizer recognizer = (Recognizer) cm.lookup("recognizer");
 	     recognizer.allocate();
-	     Result result = recognizer.recognize();
-	     System.out.println(result.getBestFinalResultNoFiller());
+
+	    
+	     Result result = null;
+	     result = recognizer.recognize();
+	    
+             String resultText = result.getBestFinalResultNoFiller();
+             System.out.println(resultText);
+	    
 	     
 	     
 	}
